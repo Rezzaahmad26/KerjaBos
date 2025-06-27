c2333a624f244b53ccc0cd1940bf5f7c96c90664
Rezza26 <rpahlevi097@gmail.com>
2025-06-27 16:05:31 +0700

    first_commit

---
+++ database/migrations/2025_06_26_150514_create_connect_topups_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       Schema::create('connect_topups', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->integer('connect_amount')->default(10); // selalu 10
+        $table->integer('price')->default(10000); // harga per 10 connect
+        $table->boolean('is_paid')->default(false); // status disetujui admin
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('connect_topups');
+    }
+};
---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Wallet;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $permissions = [
+            'manage categories',
+            'manage tools',
+            'manage projects',
+            'manage project tools',
+            'manage wallets',
+            'manage applicants',
+
+            'apply job',
+            'topup wallet',
+            'withdraw wallet',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate([
+                'name' => $permission,
+            ]);
+        }
+
+        $clientRole = Role::firstOrCreate([
+            'name' => 'project_client',
+        ]);
+        $clientPermissions = [
+            'manage projects',
+            'manage project tools',
+            'manage applicants',
+            'topup wallet',
+            'withdraw wallet',
+        ];
+        $clientRole->syncPermissions($clientPermissions);
+
+        $freelancerRole = Role::firstOrCreate([
+            'name' => 'project_freelancer',
+        ]);
+        $freelancerPermissions = [
+            'apply job',
+            'withdraw wallet',
+        ];
+        $freelancerRole->syncPermissions($freelancerPermissions);
+
+        $superAdminRole = Role::firstOrCreate([
+            'name' => 'super_admin',
+        ]);
+
+        $user = User::create([
+            'name' => 'Super Admin',
+            'email' => 'rezzaahmad57@gmail.com',
+            'occupation' => 'Owner',
+            'connect' => 999,
+            'password' => bcrypt('abcd1234'),
+            'avatar' => 'https://placehold.co/150x150/000/fff',
+        ]);
+        $user->assignRole($superAdminRole);
+
+        $wallet = new Wallet([
+            'balance' => 0,
+        ]);
+        $user->wallet()->save($wallet);
+
+    }
+}
